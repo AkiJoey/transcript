ad6aa9213a754cc582be895d7b6606171dfe03e1
AkiJoey <akijoey1010635951@gmail.com>
2020-08-06 02:12:09 +0800

    feat: footer text

--- src/components/footer.js
+++ src/components/footer.js
@@ -6,7 +6,7 @@ class Footer extends Component {
 		return (
 			<div id="footer">
 				<div id="copyright">©2019 By <a href="https://akijoey.com">AkiJoey</a></div>
-				<div id="framework">Driven <a href="https://djangoproject.com">Django</a> + <a href="https://reactjs.org">React</a></div>
+				<div id="framework">Powered by <a href="https://djangoproject.com">Django</a> + <a href="https://reactjs.org">React</a></div>
 			</div>
 		)
 	}
